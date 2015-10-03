147553372300a12987af6e25ec4d571a0e12afc2
Aritz Beobide-Cardinal <aritz@aritzcracker.ca>
2015-10-03 18:50:04 -0400

    I need 'em settings
    Signed-off-by: Aritz Beobide-Cardinal <aritz@aritzcracker.ca>

--- lua/arcphone/server/aacore.lua
+++ lua/arcphone/server/aacore.lua
@@ -3,6 +3,7 @@
 -- This shit is under copyright, and is bound to the agreement stated in the ELUA.
 -- Any 3rd party content has been used as either public domain or with permission.
 -- © Copyright 2014 Aritz Beobide-Cardinal All rights reserved.
+ARCPhone.Settings = {}
 
 ARCPhone.LogFileWritten = false
 ARCPhone.LogFile = ""
